0ad100d5ceb0081b3e14e59993cafaf0d66db4c0
garageagle <garageagle@gmail.com>
2020-03-02 09:15:13 -0600

    Remove bug

--- BattleTank/Source/BattleTank/Private/TankBarrel.cpp
+++ BattleTank/Source/BattleTank/Private/TankBarrel.cpp
@@ -2,7 +2,7 @@
 
 #include "TankBarrel.h"
 
-void UTankBarrel::Elevate(float RelativeSpeed) const
+void UTankBarrel::Elevate(float RelativeSpeed)
 {
 	RelativeSpeed = FMath::Clamp<float>(RelativeSpeed, -1.f, 1.f);
 	float ElevationChange = RelativeSpeed * MaxDegreesPerSecond * GetWorld()->DeltaTimeSeconds;
